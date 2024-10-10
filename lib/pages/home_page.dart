1951d7702a0b0a67683f0cc96173301c682c6fb0
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-10 19:19:54 +0800

    feat : get started

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  List toDoList = [
+    [
+      'Learn Flutter',
+      false
+    ],
+    [
+      'Drink Coffe',
+      false
+    ],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade300,
+      appBar: AppBar(
+        title: Text('Simpel Todo'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.deepPurple,
+                child: Text(
+                  toDoList[index][0],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
