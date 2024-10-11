88a7493393124b7ab73df5d3be0624436594e550
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-11 18:29:31 +0800

    feat : add field input data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -52,7 +52,13 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: Row(
         children: [
-          Expanded(child: TextField()),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              decoration: InputDecoration(filled: true, fillColor: Colors.deepPurple.shade200, enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(15)), focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(15))),
+            ),
+          )),
           FloatingActionButton(
             onPressed: () {},
             child: Icon(Icons.add),
